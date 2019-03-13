56f8e745ad5e1df5d63b436ec213daa9ba1512dc
Fabmaur <fabmaur2013@gmail.com>
2019-03-13 19:43:30 +0000

    Added some commenting.

--- Engine/source/core/UserApplication.cpp
+++ Engine/source/core/UserApplication.cpp
@@ -9,4 +9,35 @@ namespace core
 			if (layer->IsActive())
 				layer->onEvent(event);
 	}
+	std::function<void(EventMessage&)> UserApplication::GetEventCallBack()
+	{
+		return std::bind(&UserApplication::EventCallBack, this, std::placeholders::_1);
+	}
+	void UserApplication::Init()
+	{
+		//Initializing list of layers
+		for (auto& layer : layers)
+			layer->Init();
+	}
+
+	void UserApplication::RunMain()
+	{
+		// Running main code for list of layers
+		for (auto& layer : layers)
+			if (layer->IsActive())
+				layer->RunMain();
+	}
+	void UserApplication::PushLayer(graphics::Layer * layer)
+	{
+		layers.push_back(layer);
+	}
+	UserApplication::~UserApplication()
+	{
+		for (auto itr = layers.begin(); itr < layers.end(); itr++)
+		{
+			// Going through each layer and freeing the memory
+			delete *itr;
+			itr = layers.erase(itr);
+		}
+	}
 }
